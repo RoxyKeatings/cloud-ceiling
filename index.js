7df8bc8d75535f3381f8275d9cb701c883dcd4a6
Roxy <155686159+RoxyKeatings@users.noreply.github.com>
2024-02-29 12:17:56 -0500

    created function with API Key &URL
    calling the search-input function to access API for city weather temperature in metric units to add functionality to display current temperature of searched city

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+function displayCityTemp(response) {
+  let temperatureElement = document.querySelector("#temperature-now");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  const apiKey = "89b05tfca20b16d5f5e3c646e1oa37db";
+  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCityTemp);
+}
+
 function submittedCitySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submittedCitySearch);
