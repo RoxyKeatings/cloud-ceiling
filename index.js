3302430bf56d4bd15ea6bb976bd821ab479b3b25
Roxy <155686159+RoxyKeatings@users.noreply.github.com>
2024-02-29 13:54:04 -0500

    Injected more weather data
    added data from API into each element, formatted time and date using JS

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayCityTemp(response) {
   let temperatureElement = document.querySelector("#temperature-now");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#city");
   temperatureElement.innerHTML = Math.round(temperature);
+
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+
+  let weatherDescriptionElement = document.querySelector(
+    "#weather-description"
+  );
+  let date = new Date(response.data.time * 1000);
+  let timeElement = document.querySelector("#time-stamp");
+  timeElement.innerHTML = formatDate(date);
+
+  let clouddescription = response.data.condition.description;
+  weatherDescriptionElement.innerHTML = clouddescription;
+
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  humidityElement.innerHTML = `${humidity}%`;
+
+  let windElement = document.querySelector("#wind-speed");
+  let wind = response.data.wind.speed;
+  windElement.innerHTML = `${wind}km/h`;
+
+  let feelsLikeWeatherElement = document.querySelector("#feels-like-weather");
+  let feelsLike = response.data.temperature.feels_like;
+  feelsLikeWeatherElement.innerHTML = Math.round(feelsLike);
+  console.log(response.data);
 }
 
 function searchCity(city) {
